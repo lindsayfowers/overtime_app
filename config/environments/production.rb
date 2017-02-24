34f474d525da840b6fbc30d472f8de475bacaa0d
lindsayfowers <lfowers@gmail.com>
2017-02-24 10:59:05 -0700

    Adding mailer config requirement to production settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,4 +20,5 @@ Rails.application.configure do
   end
 
   config.active_record.dump_schema_after_migration = false
+  config.action_mailer.default_url_options = { :host => 'https://fowers-overtime.herokuapp.com/' }
 end
